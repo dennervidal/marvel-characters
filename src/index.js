dd9b24fe96d6858f8df06868b2f4efa71031b6d9
Denner <dennervidal@users.noreply.github.com>
2022-01-03 15:56:33 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./pages/App";
 import { ThemeProvider } from "@material-ui/styles";
 import { StyledThemeProvider } from "components/styled";

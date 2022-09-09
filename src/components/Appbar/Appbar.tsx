c5d3c6c9dcf84d83eae61a20420b54ede526cd60
Denner <dennervidal@users.noreply.github.com>
2022-09-08 22:12:53 -0400

    Update Appbar.tsx

--- src/components/Appbar/Appbar.tsx
+++ src/components/Appbar/Appbar.tsx
@@ -2,11 +2,14 @@ import { AppBar, Toolbar } from '@material-ui/core'
 import { LogoImg, Title } from './styled'
 import React from 'react'
 import { useRouter } from 'next/router'
+import { PaginationContext } from 'context'
 
 export const Appbar = ({ mobile }: { mobile: boolean }) => {
   const router = useRouter()
+  const { gotoPage } = useContext(PaginationContext)
   const onClickImage = () => {
     router.push('/').then()
+    gotoPage(0)
   }
 
   return (

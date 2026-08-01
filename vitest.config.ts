1f74be47ac4ded3dcd37829296e3e9d38c17a28d
Denner Vidal <denner.vidal@gmail.com>
2026-08-01 19:25:18 -0400

    feat: add prerendered character details page

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,24 @@
+/// <reference types="vitest/config" />
 import react from '@vitejs/plugin-react'
+import { getViteConfig } from 'astro/config'
 import { fileURLToPath } from 'node:url'
-import { defineConfig } from 'vitest/config'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default getViteConfig(
+  {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    test: {
+      environment: 'jsdom',
+      setupFiles: ['./src/test/setup.ts'],
+      include: ['src/**/*.test.{ts,tsx}']
     }
   },
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
-    include: ['src/**/*.test.{ts,tsx}']
+  {
+    output: 'static',
+    configFile: false
   }
-})
+)
